2bf5529cc99b9e8b197ef4802bb9c6a04e85684a
JulioHolanda <juliophc@msn.com>
2023-03-10 19:08:55 -0300

    abertura de arquivos

--- main.c
+++ main.c
@@ -1,8 +1,47 @@
+//acessar documento
+//navegar e pegar informaçõpes
+//salver informaçoes no struct
+//criar threads
+//dividir tarefas entre threads
+//criar funcoes de varredura
+//output em arquivo especifico com resposta
+//TESTES
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
-int main(){
+typedef struct{
+        int N;
+        int linha;
+        int coluna;
+        int *matriz;
+}sudoku;
 
-    return 0;
+int main(int argc, char *argv[]){
+
+        //printf("%s", argv[1]);
+        FILE *in, *out;
+        char *N, linha, coluna;
+
+
+        in = fopen(argv[1], "r");
+
+        /*
+        fscanf(in,"%c",N);
+
+        printf("%d",*N);
+        */
+
+        fclose(in);
+
+        out = fopen("sudoku_jphc.out.txt", "w");
+
+        fprintf(out, "SUCCESS");
+
+        fclose(out);
+
+
+        return 0;
 }
