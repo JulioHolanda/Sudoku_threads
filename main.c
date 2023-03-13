f084983a460dae58de96d44996a196471dea6129
JulioHolanda <juliophc@msn.com>
2023-03-13 15:46:25 -0300

    matriz em forma de array

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //acessar documento OK
 //navegar e pegar informaçõpes OK
-//salver informaçoes no struct 
+//salver informaçoes no struct OK
 //criar funcoes de varredura
 //criar threads
 //dividir tarefas entre threads
@@ -12,12 +12,18 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+void *verifica_linha(void *ptr);
+void *verifica_coluna(void *ptr);
+void *verifica_grade(void *ptr);
+
 int error = 0;
 
 typedef struct{
         int N;
+        int tot;
         int linha;
         int coluna;
+        int grades;
         int *matriz;
 }sudoku;
 
@@ -54,26 +60,35 @@ int main(int argc, char *argv[]){
         fscanf(in," %d%c%c", &n, &lixo, &lixo1);
         fscanf(in," %d%c%d", &linha, &lixo, &coluna);
         fgetc(in);
+        
         printf("N: %d\nlinha: %d\ncoluna: %d\n",n,linha,coluna);
         
-        int matriz[n][n];
-        sudoku sdk = {n, linha, coluna, &matriz[n][n]};
+        int matriz[n*n];        
 
         for(i=0; i < n; i++){
                 for(j=0; j < n ; j++){
                         //matriz[i][j] = fgetc(in) - 48;
-                        fscanf(in," %d%c",&matriz[i][j], &lixo);
+                        fscanf(in," %d%c",&matriz[(i*n) + j], &lixo);
                 }
         }
 
+        fclose(in);
+
+        sudoku sdk = {n, n*n, linha, coluna, (n*n)/(linha*coluna), matriz};
+
+        printf("sdk.N: %d\nsdk.linha: %d\nsdk.coluna: %d\nsdk.total: %d\nsdk.grades: %d\n",sdk.N,sdk.linha,sdk.coluna,sdk.tot,sdk.grades);      
+
         for(i=0; i < n; i++){
                 for(j=0; j < n ; j++){
-                        printf("%d ",matriz[i][j]);
+                        printf("%d ",sdk.matriz[(i*n) + j]);
                 }
                 printf("\n");
         }
 
-        fclose(in);
+        //inicialização de threads
+        pthread_t thread_linha[n], thread_coluna[n], thread_grade[sdk.grades]; 
+        
+        int t_linha[n], t_coluna[n], t_grade[sdk.grades];
 
         //declaracao de resultado
         out = fopen("sudoku_jphc.out.txt", "w");
